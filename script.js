87bbae7798d37d5f9f4f433607c027834fe27f21
Sophia Edeki <sophiesse143@gmail.com>
2022-06-21 11:48:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let person = {
+    name : "Sophia Edeki",
+    height :1.67,
+    country : "Nigeria"
+}
+
+alert("Hi, my name is : " + person.name);
+alert("My Height : " + person.height);
+alert("My Country : " + person.country);
