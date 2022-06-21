89164eba768421a67fd35f361890578a75d93d7c
Sophia Edeki <sophiesse143@gmail.com>
2022-06-21 12:33:40 +0100

    updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # intro_to_JS
+https://anaesse.github.io/intro_to_JS/
--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let person = {
     height :1.67,
     country : "Nigeria"
 }
-
-alert("Hi, my name is : " + person.name);
-alert("My Height : " + person.height);
-alert("My Country : " + person.country);
+ let Screen_1 = document.getElementById("screen-1");
+ let Screen_2 = document.getElementById("screen-2");
+ let Screen_3 = document.getElementById("screen-3");
+ Screen_1.textContent = "Hi, my name is : " + person.name 
+ Screen_2.textContent = "My Height : " + person.height 
+ Screen_3.textContent = "My Country : " + person.country
